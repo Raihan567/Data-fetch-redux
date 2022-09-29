2d5395aaf69bff0977653d8f2de897d28b5a9e1c
Raihan567 <aburaihansarker567@gmail.com>
2022-09-29 07:38:23 +0600

    Data-fetch-with-redux

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Todos from "./Components/Todos";
+
+// step1: install package [redux, react-redux, axios, redux-thunk]
+// step2: constant define
+// step3: async action creator
+// step4: reducer
+// step5: create a store
+// step6: provide store
+// step7: use store
+// step8: adding css
+
+function App() {
+  return (
+    <div className="App">
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
