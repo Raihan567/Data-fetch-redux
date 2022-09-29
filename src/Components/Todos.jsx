92ef81a67a2445dd31c7d040fce052da6248df6d
Raihan567 <aburaihansarker567@gmail.com>
2022-09-29 08:09:22 +0600

    data loaded successfully

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import getAllTodos from "../Services/Actions/todosActions";
 
 const Todos = () => {
+  const { isLoading, error, todos } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllTodos());
+  }, []);
   return (
     <div>
       <h1>Todo Application</h1>
+      {isLoading && <h3>Data is Loading...</h3>}
+      {error && <h3>{error}</h3>}
+      <section>
+        {todos &&
+          todos.map((todo) => {
+            return (
+              <div>
+                <h3>{todo.id}</h3>
+                <h2>{todo.title}</h2>
+              </div>
+            );
+          })}
+      </section>
     </div>
   );
 };
